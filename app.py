0734cf3a9c52e6d1c8b65fa57f946e0c64e5a753
veersanghavi2011-svg <veer.sanghavi2011@gmail.com>
2026-07-23 20:28:52 -0400

    Move homepage to HTML template

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return """
-    <h1>Veer's Homework Manager</h1>
-    <p>My first Flask website!</p>
-    """
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
